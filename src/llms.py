e1d91917a56eede1e5e3c8c439c734bb099160a0
antonioanerao <anerao.junior@gmail.com>
2026-05-31 04:57:36 -0500

    chore: melhorado nome de variaveis de ambiente para chamada de API

--- src/llms.py
+++ src/llms.py
@@ -5,17 +5,17 @@ from langchain_openai import ChatOpenAI
 load_dotenv()
 
 router_llm = ChatOpenAI(
-    model=os.getenv("SMALL_VLLM_MODEL", os.getenv("LOCAL_MODEL")),
-    base_url=os.getenv("SMALL_VLLM_BASE_URL", os.getenv("PROVIDER_URL")),
-    api_key=os.getenv("SMALL_VLLM_API_KEY", os.getenv("PROVIDER_API_KEY")),
+    model=os.getenv("ROUTER_MODEL", os.getenv("LOCAL_MODEL")),
+    base_url=os.getenv("ROUTER_BASE_URL", os.getenv("PROVIDER_URL")),
+    api_key=os.getenv("ROUTER_API_KEY", os.getenv("PROVIDER_API_KEY")),
     temperature=0.5,
     timeout=120,
 )
 
 agent_llm = ChatOpenAI(
-    model=os.getenv("VLLM_MODEL", os.getenv("LOCAL_MODEL")),
-    base_url=os.getenv("VLLM_BASE_URL", os.getenv("PROVIDER_URL")),
-    api_key=os.getenv("VLLM_API_KEY", os.getenv("PROVIDER_API_KEY")),
+    model=os.getenv("SEARCH_MODEL", os.getenv("LOCAL_MODEL")),
+    base_url=os.getenv("SEARCH_BASE_URL", os.getenv("PROVIDER_URL")),
+    api_key=os.getenv("SEARCH_API_KEY", os.getenv("PROVIDER_API_KEY")),
     temperature=0.1,
     timeout=120,
 )
